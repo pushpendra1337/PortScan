d33adae46c0c273637ebadc45c20664ff8754c88
Pushpendra Yadav <pushpendray1337@gmail.com>
2021-03-07 03:08:17 +0530

    Add initial developed files

---
+++ README.rst
@@ -0,0 +1,24 @@
+Android Port Scanner
+====================
+
+Simple Android Port Scanner written in Kotlin.
+
+Live coding of this app
+-----------------------
+
+https://www.youtube.com/watch?v=1GamiAUXeu0
+
+Source code
+-----------
+
+https://github.com/DevDungeon/AndroidPortScanner.git
+
+
+To do
+-----
+- Change 3rd tab title to 'Settings'
+- When 3rd tab is pressed, load settings layout fragment
+- In the 3rd tab, have editable settings
+	- The ip/range to scan
+	- The list of ports to scan
+
---
+++ app/src/main/java/net/intensecorp/portscan/MainActivity.kt
@@ -0,0 +1,50 @@
+package net.devdungeon.android.portscanner
+
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import com.devdungeon.android.portscanner.R
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
+import java.util.concurrent.Executors
+
+
+class MainActivity : AppCompatActivity() {
+
+    private val numThreads: Int = 16
+    private val host: String = "www.devdungeon.com"
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_startscan -> {
+                this.scrollView2.portScanResults.setText("Scan results for:\n$host\n============")
+
+                val executor = Executors.newFixedThreadPool(numThreads)
+
+                val commonlyUsedPorts = intArrayOf(22, 80, 443, 3306, 21, 25, 53, 8080, 8988, 9999)
+                for (port in commonlyUsedPorts) {
+                    val worker = PortScan(this.scrollView2.portScanResults, host, port)
+                    executor.execute(worker)
+                }
+                executor.shutdown()
+                return@OnNavigationItemSelectedListener false
+            }
+            R.id.navigation_about -> {
+                // Replace screen fragment with settings
+                // setContentView()?
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+
+}
